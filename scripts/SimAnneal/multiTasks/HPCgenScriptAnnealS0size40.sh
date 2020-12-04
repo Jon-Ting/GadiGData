0ca1f761c33761c585ea92732bcceb6d2f696c57
Jonathan Ting <jonathan.ting@anu.edu.au>
2020-12-04 16:33:28 +1100

    Updated templates and scripts in SimAnneal directory

--- scripts/SimAnneal/multiTasks/HPCgenScriptAnnealS0size40.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#PBS -P q27
-#PBS -q normal
-#PBS -l walltime=00:20:00,ncpus=1,mem=2GB,jobfs=1GB
-#PBS -l storage=gdata/q27
-#PBS -l wd
-
-bash genScriptAnnealS0size40.sh >> HPCgenScriptAnnealS0size40.log
