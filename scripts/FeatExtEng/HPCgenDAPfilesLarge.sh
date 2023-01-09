0d00211636804e0a2cc52a548f924f4a98c5ac58
Jonathan Ting <jonathan.ting@anu.edu.au>
2023-01-09 12:23:01 +1100

    Reorganised scripts for FeatExtEng/

---
+++ scripts/FeatExtEng/HPCgenDAPfilesLarge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -P q27
+#PBS -q normal
+#PBS -l walltime=10:00:00,ncpus=1,mem=5GB,jobfs=1GB,software=python
+#PBS -l storage=scratch/q27+gdata/q27
+#PBS -l wd
+
+module load python3/3.8.5
+
+python3 genDAPfilesLarge.py 
